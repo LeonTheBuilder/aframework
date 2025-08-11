8a164450fc662eed42f72054e112bcb134a92d82
chence <leoooon@foxmail.com>
2025-08-11 18:39:54 +0800

    cc

--- beans/view.renderer.bean.js
+++ beans/view.renderer.bean.js
@@ -1,10 +1,11 @@
 const ejs = require('ejs');
 const fs = require('fs').promises;
+const path = require('path');
 
 // ViewRenderer
 class Vr {
-    async render(ctx, ejsFilePath) {
-        const templateContent = await fs.readFile(ejsFilePath, 'utf8');
+    async render(ctx, viewRoot, ejsFilePath) {
+        const templateContent = await fs.readFile(path.join(viewRoot, ejsFilePath), 'utf8');
         const html = ejs.render(templateContent, {appcfg: this.cfg});
         ctx.type = 'text/html; charset=utf-8';
         ctx.body = html;
