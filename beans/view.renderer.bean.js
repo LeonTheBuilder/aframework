b222195c35a00233e64efbb890628385dd8ed1fc
chence <leoooon@foxmail.com>
2025-08-11 18:33:52 +0800

    cc

--- beans/view.renderer.bean.js
+++ beans/view.renderer.bean.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 // ViewRenderer
 class Vr {
     render(ctx, ejsFilePath) {
-        const html = ejs.render(ejsFilePath);
+        const html = ejs.render(ejsFilePath, {appcfg: this.cfg});
         ctx.type = 'text/html; charset=utf-8';
         ctx.body = html;
     }
